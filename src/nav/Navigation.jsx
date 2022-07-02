ead9a611c937135c5bca114a772e528dd9050757
kotpavlik <kotpavlik88@gmail.com>
2022-07-03 02:54:25 +0300

    refactor react-scroll

--- src/nav/Navigation.jsx
+++ src/nav/Navigation.jsx
@@ -10,7 +10,7 @@ export const Navigation = ({burger,setBurger}) => {
 
     return (
         <div className={burger ? `${style.navigation_flex} ${style.navigation_flex_burger}`: style.navigation_flex}>
-            <Link to="home" spy={true} smooth={true} offset={0} duration={500} onClick={closeBurger} >home</Link>
+            <Link to="home" spy={true} smooth={true} offset={-80} duration={500} onClick={closeBurger} >home</Link>
             <Link to="skills" spy={true} smooth={true} offset={-80} duration={500} onClick={closeBurger}>skills</Link>
             <Link to="projects" spy={true} smooth={true} offset={-80} duration={500} onClick={closeBurger}>projects</Link>
             <Link to="contacts" spy={true} smooth={true} offset={-80} duration={500} onClick={closeBurger}>contacts</Link>

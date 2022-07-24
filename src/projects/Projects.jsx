73ebe7b41cbb88c8bd30396670e3f2fd61e2ef4d
kotpavlik <kotpavlik88@gmail.com>
2022-07-24 09:51:58 +0300

    refactor project

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -4,6 +4,7 @@ import projContainer from '../common/styles/Container.module.css'
 import {Project} from './project/Project'
 import todo_list from '../assets/projects_photo/todo_list.png'
 import social_network from '../assets/projects_photo/social_network.png'
+import portfolio from '../assets/projects_photo/portfolio.png'
 
 export const Projects = () => {
 
@@ -11,15 +12,33 @@ export const Projects = () => {
         {
             id: 1,
             name: 'Social networks',
-            description: 'Social Network using React&redux technologies,API and more modules ',
+            description: 'This social network is written in React. We take the data from the Social-Network Server API https://social-network.samuraijs.com . Communication with the API is established using the AXIOS library. The social network works with pagination, login, user data processing, as well as changing and deleting user data. ',
             project_photo: `${social_network}`,
-            gh_pages:'https://github.com/kotpavlik/Ts-React-Redux'
+            gh_pages:'https://kotpavlik.github.io/Ts-React-Redux/',
+            gh_code:'https://github.com/kotpavlik/Ts-React-Redux',
+            title: 'Used Technologies',
+            technologies:
+                'React, Redux, TypeScript, React-Redux, Redux-Thunk, Axios, Lottie animation, Ant design, formik, uyp, jest tests, react-router-dom 6^, gh-pages'
         },
         {
-            id: 2, name: 'Todo List ',
-            description: 'Todo List using React&redux technologies,API and more modules ',
+            id: 2,
+            name: 'Todo List ',
+            description: 'The Todo List application is designed to control tasks. The user can create, modify, delete tasks, as well as rank them in order of importance. Describe tasks and apply markers to them. ',
             project_photo: `${todo_list}`,
-            gh_pages:'https://kotpavlik.github.io/IT-INCUBATOR-TODOLIST-TS/'
+            gh_pages:'https://kotpavlik.github.io/IT-INCUBATOR-TODOLIST-TS/',
+            gh_code:'https://github.com/kotpavlik/IT-INCUBATOR-TODOLIST-TS',
+            title:'Used Technologies',
+            technologies:'React, Redux-Toolkit, TypeScript, React-Redux, Redux-Thunk, Axios, Lottie animation, MAU design, formik, uyp, jest tests, react-router-dom 6^, gh-pages, Storybook'
+        },
+        {
+            id: 3,
+            name: 'Portfolio',
+            description: 'This rect application is written so that you can learn more about me and hire me. Uses smooth scrolling and swiper. With an adaptive for different devices and a burger menu for phones. This application shows more of my work with styles and adaptation',
+            project_photo: `${portfolio}`,
+            gh_pages:'https://kotpavlik.github.io/portfolio/',
+            gh_code:'https://github.com/kotpavlik/portfolio',
+            title:'Used Technologies',
+            technologies:'React, Lottie animation, formik, uyp, gh-pages, SASS, React-scroll , React-spring, Swiper'
         },
 
     ]
@@ -30,8 +49,7 @@ export const Projects = () => {
                 <div className={style.projects}>{myProjects.map((p) => {
                     return (
                         <Project
-                            projectName={p.name} projectDescription={p.description}
-                            project_photo={p.project_photo} key={p.id} gh_pages={p.gh_pages}/>
+                            project={p} key={p.id} />
                     )
                 })}</div>
             </div>
--- src/projects/project/Project.jsx
+++ src/projects/project/Project.jsx
@@ -1,21 +1,80 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Parallax, Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import style from './Project.module.scss'
-import {ProjectButton} from '../../components/projectButton'
+import {ProjectButton} from "../../components/projectButton";
 
 
-export const Project = ({projectName,projectDescription,project_photo,gh_pages}) => {
+
+export const Project = ({project}) => {
 const bgc_project = {
-    backgroundImage:`url(${project_photo})`
+    backgroundImage:`url(${project.project_photo})`
 }
     return (
-        <div className={style.project}>
-            <div style={bgc_project} className={style.image}>
-
-                <a href={gh_pages}><ProjectButton name={'look'}/></a>
-            </div>
-            <h3 className={style.title}>{projectName}</h3>
-            <span className={style.description}>{projectDescription}</span>
+        < div className={style.project}>
+            <Swiper
+            style={{
+                "--swiper-navigation-color": "white",
+                "--swiper-pagination-color": "white",
+            }}
+            speed={600}
+            parallax={true}
+            pagination={{
+                clickable: true,
+            }}
+            navigation={true}
+            modules={[Parallax, Pagination, Navigation]}
+        >
+            <div
+                slot="container-start"
+                className={style.parallax_bg}
+                style={bgc_project}
+                data-swiper-parallax="-23%"
+            ></div>
+            <SwiperSlide>
+                <div className={style.information_container}>
+                    <div className={style.title} data-swiper-parallax="-300">
+                        {project.name}
+                    </div>
+                    <div className={style.description} data-swiper-parallax="-100">
+                        <p>
+                            {project.description}
+                        </p>
+                    </div>
+                    <div className={style.butt_container}>
+                        <a href={project.gh_pages}><ProjectButton name={'demo'}/></a>
+                    </div>
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className={style.information_container}>
+                    <div className={style.title} data-swiper-parallax="-300">
+                        {project.title}
+                    </div>
+                    <div data-swiper-parallax="-100">
+                        <p className={style.description}>
+                            {project.technologies}
+                        </p>
+                    </div>
+                    <div>
+                        <a href={project.gh_code}><ProjectButton name={'code'}/></a>
+                    </div>
+                </div>
+            </SwiperSlide>
+            </Swiper>
         </div>
+
     );
 };
 
+            // <div className={style.project}>
+                //     <div style={bgc_project} className={style.image}>
+                //
+                //
+                //     </div>
+                //     <h3 className={style.title}>{projectName}</h3>
+                //     <span className={style.description}>{projectDescription}</span>
+                // </div>
